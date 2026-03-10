52c8f4904b6aa99c8f826180055c6023da68cf89
Dung-vie <dunghoang1122004@gmail.com>
2026-03-10 23:37:30 +0700

    xoa file rac

--- src/test/java/fit/tdc/QuanLyTaiLieu/QuanLyTaiLieuApplicationTests.java
+++ src/test/java/fit/tdc/QuanLyTaiLieu/QuanLyTaiLieuApplicationTests.java
@@ -1,10 +1,10 @@
-package fit.tdc.de3DoAn1Java2;
+package fit.tdc.QuanLyTaiLieu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class De3DoAn1Java2ApplicationTests {
+class QuanLyTaiLieuApplicationTests {
 
 	@Test
 	void contextLoads() {
